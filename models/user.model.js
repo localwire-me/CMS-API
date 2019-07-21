9e6f12782b47c870375601250dd1272550ce7acd
Ritesh Sharma <sharmaritesh3312@gmail.com>
2019-07-21 13:03:52 +0530

    User Model created

--- models/user.model.js
+++ models/user.model.js
@@ -1,11 +1,40 @@
-const mongoose = require('mongoose')//Importing mongose DB
-const Schema= mongoose.Schema;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-//Define schema
-let userSchema=new Schema({
-    email_id:{type:String,required:true,max:50},
-    password:{type:String,required:true}
-})
+const bcrypt = require('bcrypt-nodejs')
 
-//Export the model
-module.exports=mongoose.model('User',userSchema)
+const UserSchema = new Schema({
+  email: { type: String, unique: true, lowercase: true },
+  username: { type: String, unique: true, lowercase: true },
+  name: String,
+  password: String,
+  picture: String,
+  address: {
+    addr1: String,
+    addr2: String,
+    city: String,
+    state: String,
+    country: String,
+    postalCode: String
+  },
+  created: { type: Date, default: Date.now }
+});
+
+UserSchema.pre('save', function(next) {
+  var user = this;
+
+  if (!user.isModified('password')) return next();
+
+  bcrypt.hash(user.password, null, null, function(err, hash) {
+    if (err) return next(err);
+
+    user.password = hash;
+    next();
+  });
+});
+
+UserSchema.methods.comparePassword = function(password) {
+  return bcrypt.compareSync(password, this.password);
+}
+
+module.exports = mongoose.model('User', UserSchema);
